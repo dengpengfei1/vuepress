6dbdf2d1f33c5a7b1ae4c3b8e21cc5276e40a758
dengpengfei <dengpengfei@360.net>
2018-05-04 17:49:18 +0800

    去掉fui 的 css

---
+++ docs/.vuepress/dist/assets/js/1.4678ec10.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{62:function(t,n,s){"use strict";s.r(n);var e=s(0),i=Object(e.a)({},function(){var t=this.$createElement,n=this._self._c||t;return n("div",{staticClass:"content"},[n("h1",{attrs:{id:"version-2"}},[this._v("version-2")]),n("FuiVersion2")],1)},[],!1,null,null,null);n.default=i.exports}}]);
---
+++ docs/.vuepress/dist/assets/js/2.e041006d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{67:function(t,n,s){"use strict";s.r(n);var e=s(0),i=Object(e.a)({},function(){var t=this.$createElement,n=this._self._c||t;return n("div",{staticClass:"content"},[n("h1",{attrs:{id:"version-1"}},[this._v("version-1")]),n("FuiVersion1")],1)},[],!1,null,null,null);n.default=i.exports}}]);
